c76cb6c537b522d049947243bb634d4972ea3c1b
Meseery <m.elmeseery@meunity.com>
2016-07-28 13:54:33 +0200

    Initial Commit

---
+++ BSTree/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BSTree
+//
+//  Created by Mohamed EL Meseery on 7/28/16.
+//  Copyright © 2016 Meseery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
